cef43ba53e37f4396b80840817acdb45a19884b0
Chiszpo-Code <cr.hernndez@ebc.edu.mx>
2026-08-30 11:27:04 -0600

    Initial commit

---
+++ proyecto/todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Tarea(models.Model):
+    tarea = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.tarea
+    
